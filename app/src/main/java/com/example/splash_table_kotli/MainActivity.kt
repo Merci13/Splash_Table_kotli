8ffae243af58d1c4e5a44f2bbe4a979ee77662f8
Merci13 <mrjorxe@gmail.com>
2019-09-23 16:33:19 -0600

    Termino de la ventana splash
    Se termino la funcion de splash del main activity y se creo el home activity

--- app/src/main/java/com/example/splash_table_kotli/MainActivity.kt
+++ app/src/main/java/com/example/splash_table_kotli/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.example.splash_table_kotli
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import androidx.core.os.postDelayed
+
+//----constantes----//
+private const val SPLASH_TIME_OUT: Long = 5000
+
 
 class MainActivity : AppCompatActivity() {
 
-    private  val SPLASH_TIME_OUT: Int = 2000;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /**
+         * EL handler post delayed me permite retrasar el llamado del home activity
+         *
+         *
+         * */
+        Handler().postDelayed({
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }, SPLASH_TIME_OUT)
+
     }
+
+
 
 }
